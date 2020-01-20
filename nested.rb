aea556178a4ddc5f1a4667966287d1b9d0c0a028
Zach Brown <zachmb22@gmail.com>
2020-01-20 23:46:43 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     return programmer_hash[:dennis_ritchie][:languages].join("")
 end
 
 def adding_matz
@@ -79,8 +81,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+     
+programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+}
+    programmer_hash
 end
 
 def changing_alan
@@ -101,6 +107,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
      
      
 end
@@ -121,5 +129,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+     programmer_hash
 
 end
