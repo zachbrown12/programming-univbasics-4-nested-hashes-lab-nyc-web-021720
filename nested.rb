844913949070c5623ee1a5f48fc62c1de67f6322
Zach Brown <zachmb22@gmail.com>
2020-01-20 23:44:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,10 +85,8 @@ def adding_matz
 programmer_hash[:yukihiro_matsumoto] = {
   :known_for => "Ruby",
    :languages => ["LISP", "C"]
-   
-   programmer_hash
 }
-    
+    programmer_hash
 end
 
 def changing_alan
